ec0c28b09bc7f8e27690da46dbdd765439d9eb0c
Leticia Gonzalez <Leticia_Gonzalez@lseg.com>
2023-05-02 15:24:42 -0600

    Add project files.

---
+++ CatalogService.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CatalogService.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ CatalogService.Infraestructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CatalogService.Infraestructure
+{
+    public class Class1
+    {
+    }
+}
